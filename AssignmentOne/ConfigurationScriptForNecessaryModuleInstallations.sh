1704908361a04172e1c6b0dad20834087e7bf819
Daniel Ruatta <druatta@gmail.com>
2018-09-17 04:05:34 +0000

    Able to import DataFrame from pandas

--- AssignmentOne/ConfigurationScriptForNecessaryModuleInstallations.sh
+++ AssignmentOne/ConfigurationScriptForNecessaryModuleInstallations.sh
@@ -1,6 +1,4 @@
-#! /usr/bin/env bash
-
-# Install the SciPy stack on Amazon Linux and prepare it for AWS Lambda
+echo -e "Beginning configuration.\n"
 
 sudo yum -y update
 sudo yum -y groupinstall "Development Tools"
@@ -15,8 +13,12 @@ sudo yum -y install python27-scipy.x86_64
 sudo yum -y install python27-matplotlib.x86_64
 
 cd /home/ec2-user
-pip install pandas -t /home/ec2-user/pandas
-pip install sympy -t /home/ec2-user/sympy
+
+hash -r
+pip install --user --upgrade --force-reinstall pip
+pip install wheel --user
+pip install --user --ignore-installed pandas
+pip install -I --upgrade sympy -t /home/ec2-user/sympy
 
 cp -R /usr/lib64/python2.7/dist-packages/numpy /home/ec2-user/numpy
 cp -R /usr/lib64/python2.7/dist-packages/scipy /home/ec2-user/scipy
@@ -24,4 +26,5 @@ cp -R /usr/lib64/python2.7/dist-packages/matplotlib /home/ec2-user/matplotlib
 
 # need the supporting libraries in place on the desintation host
 tar -czvf scipy-stack.tgz /home/ec2-user/*
-# could easily edit to copy to S3 or push to a repo
+
+echo -e "\nConfiguration complete."
--- AssignmentOne/ReadExcelSheet.py
+++ AssignmentOne/ReadExcelSheet.py
@@ -1,1 +1,6 @@
 import numpy as np
+import matplotlib.pyplot as plt
+from pandas import DataFrame
+
+
+print("Hello, world!")
